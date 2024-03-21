e19839dc252881c5b66e9edecf86d662d136fdc8
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-03-21 11:20:40 +0400

    Tic Tac Toe xo.py

--- xo.py
+++ xo.py
@@ -63,7 +63,7 @@ def check_winner(board):
     if board[0][0] == board[1][1] == board[2][2] != '-':
         return board[0][0]
     if board[0][2] == board[1][1] == board[0][2] != '-':
-        return [0][2]
+        return board[0][2]
 
     return None
 
